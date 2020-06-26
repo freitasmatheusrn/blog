f48ff3bec25debe2d9a33f39f13ef78dcd5d6348
freitasmatheusrn <freitasmatheusrn@gmail.com>
2020-06-26 17:00:40 -0300

    aplicação rodando

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :contatos
  
-  root :to => contatos_path
+  root :to => 'contatos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
