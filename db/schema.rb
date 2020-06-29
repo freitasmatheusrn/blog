cbb32b7cc43c8b3e799565fef8593000ed7a3085
freitasmatheusrn <freitasmatheusrn@gmail.com>
2020-06-29 14:46:34 -0300

    agora é um blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_26_191111) do
+ActiveRecord::Schema.define(version: 2020_06_29_160814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "contatos", force: :cascade do |t|
     t.string "nome"
