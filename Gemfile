4aa1c286b224f5d5a4e1988a4f3789860395d265
Tiago Tobias <tiagotobias2003@gmail.com>
2020-07-02 17:11:46 -0300

    set devise

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'jbuilder', '~> 2.5'
 
 #my gems
 gem 'simple_form' #easy forms 
+gem 'devise' #flexible authentication
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
   resources :articles do 
     post 'new_comment', on: :member
   end
   
   get "pages/:id" => "pages#index", :as => :pages
-  root :to => 'pages#home'
+  root 'pages#index', {id: "home"}
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
