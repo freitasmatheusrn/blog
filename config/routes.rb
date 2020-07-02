4f9270f9d3704efa0ec2931e577cba1bc4c44b9f
freitasmatheusrn <freitasmatheusrn@gmail.com>
2020-07-02 17:10:19 -0300

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   resources :articles do 
     post 'new_comment', on: :member
   end
   
   get "pages/:id" => "pages#index", :as => :pages
-  root :to => 'pages#home'
+  root 'pages#index', {id: "home"}
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
